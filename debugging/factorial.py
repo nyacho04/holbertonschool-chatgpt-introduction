d3b303981468b8ad638545751850c0895e31352f
nyacho04 <dnachod83@gmail.com>
2025-01-15 11:08:38 -0300

    task 0 chatgpt introduction

---
+++ debugging/factorial.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def factiorial(n):
+    if n == 1:
+        while n > 1:
+            result *= n
+            n -= 1
+            return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
